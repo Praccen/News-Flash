008a61b174a20255cbd8950bcef06a127fdc355e
Anton <anton_christoffersson@hotmail.com>
2023-04-30 23:39:43 +0200

    Added echos for better logging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 npm install
+echo "Compiling typescript..."
 ./node_modules/.bin/tsc -p tsconfig.json
+echo "Recreate deploy dir..."
 rm -r /home/shaggy/public
 mkdir /home/shaggy/public
+echo "Copying files..."
 cp * /home/shaggy/public/ -r
